32ef8b4d5264a5a3a21f8b76e742aa1390bbff9f
Freelancer • Programmer • Pentester • Security Enthusiast <32599376+bitPanG98@users.noreply.github.com>
2018-10-04 11:44:50 -0500

    Create FactorialSumas.java

---
+++ Factorial/FactorialSumas.java
@@ -0,0 +1,38 @@
+package factorialsumas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bitPanG98
+ * @fecha : 04-10-2018
+ * @github : https://github.com/bitPanG98
+ */
+public class Factoriaal {
+
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("-> Ingrese el numero : ");
+        int numero = entrada.nextInt();
+
+        int suma = 0;
+        int factorial = 1;
+
+        for (int i = 2; i <= numero; i++) {
+
+            for (int j = 1; j <= factorial; j++) {
+                suma = suma + i;
+            }
+
+            factorial = suma;
+            suma = 0;
+        }
+
+        System.out.println("El factorial de ["+numero+"] es -> " + factorial);
+
+    }
+
+}
